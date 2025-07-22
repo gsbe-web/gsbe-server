ed520f20778a48828fe80f7f014671c78d9f7448
sageofsixpacks <yahyayakub900@gmail.com>
2025-07-22 11:59:47 +0000

    implemented updateMember

--- src/members/dto/update.dto.ts
+++ src/members/dto/update.dto.ts
@@ -2,4 +2,6 @@ import { PartialType } from '@nestjs/swagger';
 
 import { CreateMemberDto } from './create.dto';
 
-export class UpdateMemberDto extends PartialType(CreateMemberDto) {}
+export class UpdateMemberDto extends PartialType(CreateMemberDto) {
+  title: any;
+}
