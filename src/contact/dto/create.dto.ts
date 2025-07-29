da25c8bcbc8c3e2959bfb09698b107f51296d662
EbenDomey <domeyeben4@gmail.com>
2025-07-29 15:49:16 +0000

    fix(contacts): faker import

--- src/contact/dto/create.dto.ts
+++ src/contact/dto/create.dto.ts
@@ -1,4 +1,4 @@
-import { faker } from '@faker-js/faker/.';
+import { faker } from '@faker-js/faker';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsPhoneNumber } from 'class-validator';
 
