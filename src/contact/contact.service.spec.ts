79ef94837eebb96497b8309e1192cc1790d1c8bc
EbenDomey <domeyeben4@gmail.com>
2025-07-30 13:05:56 +0000

    feat(contact): included mail service

--- src/contact/contact.service.spec.ts
+++ src/contact/contact.service.spec.ts
@@ -1,16 +1,41 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { EmailService } from '@notifications/email/email.service';
+import { DeepMockProxy, mockDeep, mockReset } from 'jest-mock-extended';
 
 import { ContactService } from './contact.service';
 
 describe('ContactService', () => {
   let service: ContactService;
+  let configServiceMock: DeepMockProxy<ConfigService>;
+  let mailServiceMock: DeepMockProxy<EmailService>;
 
   beforeEach(async () => {
+    configServiceMock = mockDeep<ConfigService>();
+    mailServiceMock = mockDeep<EmailService>();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ContactService],
+      providers: [
+        ContactService,
+        {
+          provide: ConfigService,
+          useValue: configServiceMock,
+        },
+        {
+          provide: EmailService,
+          useValue: mailServiceMock,
+        },
+      ],
     }).compile();
 
     service = module.get<ContactService>(ContactService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+    mockReset(configServiceMock);
+    mockReset(mailServiceMock);
   });
 
   it('should be defined', () => {
--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -1,13 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { EmailService } from '@notifications/email/email.service';
 
 import { CreateContactDto } from './dto';
 
 @Injectable()
 export class ContactService {
+  constructor(
+    private configService: ConfigService,
+    private mailService: EmailService,
+  ) {}
+
   async create(dto: CreateContactDto) {
+    this.sendMessage(dto);
     return dto;
   }
 
+  private sendMessage(payload: CreateContactDto) {
+    const email = {
+      from: `${payload.email}<${this.configService.get<string>('EMAIL_USERNAME')}>`,
+      to: this.configService.get<string>('EMAIL_TO'),
+      subject: payload.subject,
+      template: './sendMessage',
+      context: payload,
+      replyTo: payload.email,
+    };
+    this.mailService.send(email);
+  }
   async findAll() {
     return `This action returns all contact`;
   }
